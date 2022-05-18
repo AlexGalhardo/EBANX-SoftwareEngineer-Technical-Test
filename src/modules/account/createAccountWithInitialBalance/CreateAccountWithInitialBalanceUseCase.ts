720c4485ba280a1d1eaa6152f5ab4dad387ff6ea
Alex Galhardo <aleexgvieira@gmail.com>
2022-05-18 20:51:50 -0300

    refactor: some refactoring in controller, routes, and repository initial boilerplate

--- src/modules/account/createAccountWithInitialBalance/CreateAccountWithInitialBalanceUseCase.ts
+++ src/modules/account/createAccountWithInitialBalance/CreateAccountWithInitialBalanceUseCase.ts
@@ -1,11 +1,31 @@
 import inMemoryJSONDatabase from '../../../repositories/inMemoryJSON';
+import fs from 'fs-extra'
 interface ICreateAccountWithInitialBalance {
     type: string;
     destination: string;
     amount: number;
 }
 export class CreateAccountWithInitialBalanceUseCase {
+    // private readonly accountRepository: AccountRepository
+
     async execute({ type, destination, amount }: ICreateAccountWithInitialBalance) {
 
+        const newAccountWithInitialBalanceObject: ICreateAccountWithInitialBalance = {
+            type,
+            destination,
+            amount
+        }
+
+        // this.accountRepository.createAccountWithInitialBalance(newAccountWithInitialBalanceObject)
+
+        inMemoryJSONDatabase.destination = newAccountWithInitialBalanceObject
+
+        fs.writeFileSync('../../../repositories/inMemoryJSONDatabase.json', JSON.stringify(inMemoryJSONDatabase, null, 2), error => {
+            if (error) {
+                throw new Error(error);
+            }
+        });
+
+        return inMemoryJSONDatabase
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
+import { CreateAccountWithInitialBalanceController } from "./modules/account/createAccountWithInitialBalance/CreateAccountWithInitialBalanceController";
+
 const routes = Router()
 
 
 export default routes
-    .post('/reset',)
-    .post('/event')
-    .get('')
-    .get('')
+    // .post('/reset',)
+    .post('/event', CreateAccountWithInitialBalanceController)
+    // .get('')
+    // .get('')
