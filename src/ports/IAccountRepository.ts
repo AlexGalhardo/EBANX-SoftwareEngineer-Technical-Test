b954f6c9bea0a2b51adecf82ade266d4d6aa8e98
Alex Galhardo <aleexgvieira@gmail.com>
2022-05-20 12:21:22 -0300

    refactor: improved IAccountRepository logic code

--- src/ports/IAccountRepository.ts
+++ src/ports/IAccountRepository.ts
@@ -1,7 +1,32 @@
+type typeDepositMessage = {
+    destination: {
+        id: string,
+        balance: number
+    }
+}
+
+type typeWithdrawMessage = {
+    origin: {
+        id: string,
+        balance: number
+    }
+}
+
+type typeTransferMessage = {
+    origin: {
+        id: string,
+        balance: number
+    },
+    destination: {
+        id: string,
+        balance: number
+    }
+}
+
 export interface IAccountRepository {
     resetStateBeforeStartingTests(): void
-    getBalance(account_id: string): { httpStatusCodeResponse: number, message: string }
-    deposit(destination: string, amount: number): { httpStatusCodeResponse: number, message: string }
-    withdraw(destination: string, amount: number): { httpStatusCodeResponse: number, message: string }
-    transfer(destination: string, amount: number): { httpStatusCodeResponse: number, message: string }
+    getBalance(account_id: string): { httpStatusCodeResponse: number, message: typeDepositMessage }
+    deposit(destination: string, amount: number): { httpStatusCodeResponse: 201, message: typeDepositMessage }
+    withdraw(destination: string, amount: number): { httpStatusCodeResponse: 201 | 404, message: 0 | typeWithdrawMessage }
+    transfer(destination: string, amount: number): { httpStatusCodeResponse: 201 | 404, message: 0 | typeTransferMessage }
 }
